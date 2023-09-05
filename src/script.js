7fd183f4f6555736948b17e69bd03d6ea21b7c7b
2701kai <2701kai@gmail.com>
2023-09-05 14:01:14 +0200

    update

--- src/script.js
+++ src/script.js
@@ -3,11 +3,30 @@
 //   menu.classList.toggle("hidden");
 // });
 
-document.addEventListener("DOMContentLoaded", function () {
-  const menuToggle = document.getElementById("menuToggle");
+// document.addEventListener("DOMContentLoaded", function () {
+//   const menuToggle = document.getElementById("menuToggle");
 
+//   menuToggle.addEventListener("click", function () {
+//     const menu = document.getElementById("menu");
+//     menu.classList.toggle("hidden");
+//   });
+// });
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   const menu = document.getElementById("menu");
+//   const menuToggle = document.getElementById("menu-toggle");
+//   menuToggle.addEventListener("click", function () {
+//     menu.classList.toggle("hidden");
+//   });
+// });
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Get the menu and the toggle button elements
+  const menu = document.getElementById("menu");
+  const menuToggle = document.getElementById("menu-toggle");
+
+  // Toggle the 'hidden' class on menu when the toggle button is clicked
   menuToggle.addEventListener("click", function () {
-    const menu = document.getElementById("menu");
     menu.classList.toggle("hidden");
   });
 });
